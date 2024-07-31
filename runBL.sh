0528366abd1f8ee0f4b5d1dcea201f4bd038bd15
pkufzc <85997924+pkufzc@users.noreply.github.com>
2024-07-31 17:52:15 +0800

    Update runBL.sh

--- runBL.sh
+++ runBL.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 
-cd workload_gen; ./workload_gen int 5000000 64 1000000 64 0.0 1 uniform uniform; cd ..
+cd workload_gen; ./workload_gen int 50000000 64 10000000 64 0.0 1 uniform uniform; cd ..
 
 echo "=============================================================================="
-cd src; ./BL_workload_simd int 5000000 1000000 26 1 1 REncoder; cd ..
+cd src; ./BL_workload_simd int 50000000 10000000 26 1 1 REncoder; cd ..
 echo "=============================================================================="
 cd src; ./BL_workload_simd int 50000000 10000000 26 1 1 REncoderSS; cd ..
 echo "=============================================================================="
